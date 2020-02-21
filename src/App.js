ca0ffd7e3cd4b87f394b10a198c2554663fb996f
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-02-20 22:36:11 -0300

    :art: Adiciona Euro

--- src/App.js
+++ src/App.js
@@ -7,8 +7,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
+        <div className="container">
+          <div className="row">
+            <div className="col s6">
+              <div className="card hoverable">
+                <Conversor moedaA="USD" moedaB="BRL" />
+              </div>
+            </div>
 
-        <Conversor moedaA="USD" moedaB="BRL" />
+            <div className="col s6">
+              <div className="card hoverable">
+              <Conversor moedaA="BRL" moedaB="USD" />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col s6">
+              <div className="card hoverable">
+                <Conversor moedaA="EUR" moedaB="BRL" />
+              </div>
+            </div>
+
+            <div className="col s6">
+              <div className="card hoverable">
+              <Conversor moedaA="BRL" moedaB="EUR" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
 
       </div>
     );
